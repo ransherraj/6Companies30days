69b93485d997cbd390a555432bff2a03b0723061
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2022-01-05 03:24:26 +0530

    Create 10. max 10 numbers in a list having 10M entries.cpp

---
+++ goldman sachs/10. max 10 numbers in a list having 10M entries.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    priority_queue<int> pq;
+    for(int i = 0; i<n; i++){
+        int temp;
+        cin>>temp;
+        pq.push(temp);
+    }
+    int count = 0;
+    while(!pq.empty() && count <= 10){
+        cout<<pq.top()<<" ";
+        pq.pop();
+        count++;
+    }
+    return 0;
+}
